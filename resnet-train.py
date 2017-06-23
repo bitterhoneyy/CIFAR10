e4761da4fbf698a50c257e1db69e274d14642f7b
bitterhoney <641150962@qq.com>
2017-06-22 23:41:47 -0500

    Update resnet-train.py

--- resnet-train.py
+++ resnet-train.py
@@ -21,49 +21,46 @@ img_aug.add_random_flip_leftright()
 img_aug.add_random_crop([32, 32], padding=4)
 
 #Trainning
-network = tflearn.input_data(shape=[None, 32, 32, 3],
-                         data_preprocessing=img_prep,
-                         data_augmentation=img_aug)
 
+network = conv_2d(network, 8, 3, activation='relu')
+network = conv_2d(network, 8, 3, activation='relu')
+network = max_pool_2d(network, 2 ,strides=2)
+
+network = conv_2d(network, 16, 3, activation='relu')
+network = conv_2d(network, 16, 3, activation='relu')
+network = max_pool_2d(network, 2 ,strides=2)
+
+network = conv_2d(network, 32, 3, activation='relu')
 network = conv_2d(network, 32, 3, activation='relu')
 network = conv_2d(network, 32, 3, activation='relu')
 network = max_pool_2d(network, 2 ,strides=2)
 
 network = conv_2d(network, 64, 3, activation='relu')
 network = conv_2d(network, 64, 3, activation='relu')
+network = conv_2d(network, 64, 3, activation='relu')
 network = max_pool_2d(network, 2 ,strides=2)
 
-network = conv_2d(network, 128, 3, activation='relu')
-network = conv_2d(network, 128, 3, activation='relu')
-network = conv_2d(network, 128, 3, activation='relu')
+network = conv_2d(network, 64, 3, activation='relu')
+network = conv_2d(network, 64, 3, activation='relu')
+network = conv_2d(network, 64, 3, activation='relu')
 network = max_pool_2d(network, 2 ,strides=2)
 
-network = conv_2d(network, 256, 3, activation='relu')
-network = conv_2d(network, 256, 3, activation='relu')
-network = conv_2d(network, 256, 3, activation='relu')
-network = max_pool_2d(network, 2 ,strides=2)
-
-network = conv_2d(network, 256, 3, activation='relu')
-network = conv_2d(network, 256, 3, activation='relu')
-network = conv_2d(network, 256, 3, activation='relu')
-network = max_pool_2d(network, 2 ,strides=2)
-
-network = fully_connected(network, 2032, activation='relu')
+network = fully_connected(network, 2056, activation='relu')
 network = dropout(network,0.5)
-network = fully_connected(network, 2032, activation='relu')
+network = fully_connected(network, 2056, activation='relu')
 network = dropout(network,0.5)
 network = fully_connected(network, 10, activation='softmax')
 # Regression
 
 mom = tflearn.Momentum(0.1, lr_decay=0.1, decay_step=32000, staircase=True)
 network = tflearn.regression(network, optimizer=mom,
-                         loss='categorical_crossentropy')
+                         loss='categorical_crossentropy',learning_rate=0.001)
 # Training
 model = tflearn.DNN(network, checkpoint_path='model_resnet_cifar10',
                     max_checkpoints=10, tensorboard_verbose=0,
                     clip_gradients=0.)
 
-model.fit(X, Y, n_epoch=200, validation_set=(testX, testY),
-          snapshot_epoch=False, snapshot_step=500,
+model.fit(X, Y, n_epoch=20, validation_set=(testX, testY),
+          snapshot_epoch=False, snapshot_step=50,
           show_metric=True, batch_size=128, shuffle=True,
           run_id='resnet_cifar10')
